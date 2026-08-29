7c239335a5ed96784f2f564583103cc282ae5754
pepito-peterson <20190018209@my.xu.edu.ph>
2026-08-30 00:49:51 +0800

    Updated HTML, CSS, JS, and README.md

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# **My Activity 2 for ITCC 41 CCA | Mobile App Development**
+# **Activity 3: Responsive Student Profile**
 
 
 
@@ -12,7 +12,31 @@
 
 
 
-Screenshots of my project launching my Student Profile App on an AVD:
+Screenshots of my project launching my Student Profile App on an AVD (Mobile, Tablet) as well on Desktop:
 
-![Cordova App Screenshot](StudentProfileSS1.jpg) 
-![Cordova App Screenshot](StudentProfileSS2.png) 
+Mobile
+
+Vertical
+![Cordova App Screenshot](MobileVertical1.png)
+![Cordova App Screenshot](MobileVertical2.png)
+![Cordova App Screenshot](MobileVertical3.png)
+
+Horizontal
+![Cordova App Screenshot](MobileHorizontal1.png)
+![Cordova App Screenshot](MobileHorizontal2.png)
+![Cordova App Screenshot](MobileHorizontal3.png)
+
+Tablet
+
+Vertical
+![Cordova App Screenshot](TabletVertical1.png)
+![Cordova App Screenshot](TabletVertical2.png)
+
+Horizontal
+![Cordova App Screenshot](TabletHorizontal1.png)
+![Cordova App Screenshot](TabletHorizontal2.png)
+
+Desktop
+![Cordova App Screenshot](Desktop1.png)
+![Cordova App Screenshot](Desktop2.png) 
+
--- www/js/index.js
+++ www/js/index.js
@@ -19,38 +19,9 @@
 /* --- initialize cordova --- */
 // Wait for the deviceready event before using any of Cordova's device APIs.
 // See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
+
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
-
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-
-    //safety check added in case the 'deviceready' element was removed from the new HTML//
-    const readyElement = document.getElementById('deviceready');
-    if (readyElement) {
-        readyElement.classList.add('ready');
-    }
 }
-
-/* --- app ui --- */
-document.addEventListener('DOMContentLoaded', () => {
-    const navButtons = document.querySelectorAll('.nav-btn');
-    const views = document.querySelectorAll('.view');
-
-    navButtons.forEach(button => {
-        button.addEventListener('click', () => {
-            //remove active class from all buttons//
-            navButtons.forEach(btn => btn.classList.remove('active'));
-
-            //add 'active' class to the clicked button
-            button.classList.add('active');
-
-            //hide all views//
-            views.forEach(view => view.classList.remove('active'));
-
-            //show the target view based on the button's data-target attribute//
-            const targetId = button.getAttribute('data-target');
-            document.getElementById(targetId).classList.add('active');
-        });
-    });
-});
